69bff564bf50dc04f7d529e6970a17b9b53b3d2d
Your Name <you@example.com>
2025-04-05 19:57:48 +0700

    update fitur + dark mode

--- dist/src/js/script.js
+++ dist/src/js/script.js
@@ -1,20 +1,38 @@
 //scroll
-window.onscroll= function(){
-    const header = document.getElementById("nav")
-    const scroll = header.offsetTop
-    if(window.pageYOffset > scroll){
-        header.classList.add("sticky")
-    }
-    else {
-        header.classList.remove("sticky")
-    }
-}
 
+window.onscroll = function () {
+  const header = document.getElementById("nav");
+  const scroll = header.offsetTop;
+  const btnUp = document.getElementById("btnUp");
+  if (window.pageYOffset > scroll) {
+    header.classList.add("sticky");
+    btnUp.classList.add("flex");
+    btnUp.classList.remove("hidden");
+  } else {
+    header.classList.remove("sticky");
+    btnUp.classList.remove("flex");
+    btnUp.classList.add("hidden");
+  }
+};
 
 // humburger
-const humburger = document.getElementById("hamburger")
-const nav = document.getElementById("nav-menu")
-humburger.addEventListener("click",()=>{
-    humburger.classList.toggle("humberger-active")
-    nav.classList.toggle("hidden")
-})
+const humburger = document.getElementById("hamburger");
+const nav = document.getElementById("nav-menu");
+humburger.addEventListener("click", () => {
+  humburger.classList.toggle("humberger-active");
+  nav.classList.toggle("hidden");
+});
+
+// klik di luar menu
+window.addEventListener("click", (e) => {
+  if (e.target !== humburger && e.target !== nav) {
+    humburger.classList.remove("humberger-active");
+    nav.classList.add("hidden");
+  }
+});
+// dark modul
+const darkMode = document.getElementById("dark-toggle");
+const html = document.querySelector("html");
+darkMode.addEventListener("click", () => {
+  html.classList.toggle("dark");
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,21 +4,21 @@ module.exports = {
     "./index.html", // Menambahkan file index.html di root
     "./src/**/*.{html,js}", // Jika Anda memiliki file HTML atau JS di dalam folder src
   ],
+  darkMode: "class",
   theme: {
-    container:{
-      center:true,
-      padding:'16px',
+    container: {
+      center: true,
+      padding: "16px",
     },
     extend: {
       colors: {
         primary: "oklch(0.623 0.214 259.815)",
-        secondary:"oklch(0.551 0.027 264.364)"
+        secondary: "oklch(0.551 0.027 264.364)",
       },
-      screens:{
-        "2xl":"1320px"
+      screens: {
+        "2xl": "1320px",
       },
-      
     },
   },
   plugins: [],
-}
+};
